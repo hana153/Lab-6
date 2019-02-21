078be3a7936b6fd6c8ee9201707417848a5c690e
Hana <hanabananak@gmail.com>
2019-02-21 16:12:44 -0600

    Completed Square and Rectangle classes

--- src/Square.java
+++ src/Square.java
@@ -2,9 +2,9 @@
 public class Square extends Rectangle
 {
 	//constructor to create a Square object
-	public Square(double size) 
+	public Square(String id, double size) 
 	{
-		
+		super(id, size, size);
 	}
 	
 	//returns the type of shape that was created and used, ie. square
